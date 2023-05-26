f6ecc248a918b4c6eab314ee3da5e67bef082ee0
ehmenzala <ehmenzala@gmail.com>
2023-05-25 20:28:10 -0500

    Modify data persistence class & add book.psv file
    psv stands for Pipe Separated Value

--- src/esteb/Book.java
+++ src/esteb/Book.java
@@ -1,29 +1,36 @@
 package esteb;
 
 public class Book {
+    private int id;
     private String title;
     private String author;
     private String genre;
     private int numberOfPages;
-    private int publishDate;
+    private int publishedDate;
     private String language;
     private String isbn;
     private int rating; // From 1 to 5
     private String fragment;
 
-    public Book(String title, String author, String genre, int numberOfPages, int publishDate, String language, String isbn, int rating, String fragment) {
+    public Book(int id, String title, String author, String genre, int numberOfPages, int publishDate, String language, String isbn, int rating, String fragment) {
+        this.id = id;
         this.title = title;
         this.author = author;
         this.genre = genre;
         this.numberOfPages = numberOfPages;
-        this.publishDate = publishDate;
+        this.publishedDate = publishDate;
         this.language = language;
         this.isbn = isbn;
         this.rating = rating;
         this.fragment = fragment;
     }
     
-    public Book() {
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -58,12 +65,12 @@ public class Book {
         this.numberOfPages = numberOfPages;
     }
 
-    public int getPublishDate() {
-        return publishDate;
+    public int getPublishedDate() {
+        return publishedDate;
     }
 
-    public void setPublishDate(int publishDate) {
-        this.publishDate = publishDate;
+    public void setPublishedDate(int publishedDate) {
+        this.publishedDate = publishedDate;
     }
 
     public String getLanguage() {
---
+++ src/esteb/BookRepository.java
@@ -0,0 +1,111 @@
+package esteb;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookRepository {
+    
+    private final String FILE_PATH = "./src/db/books.psv";
+
+    public BookRepository() {
+    }
+
+    public void createBook(Book book) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_PATH, true))) {
+            String bookData = getBookDataString(book);
+            writer.println(bookData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Book> readAllBooks() {
+        ArrayList<Book> books = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Book book = parseBookData(line);
+                books.add(book);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return books;
+    }
+
+    public void updateBook(Book updatedBook) {
+        List<Book> books = readAllBooks();
+
+        for (int i = 0; i < books.size(); i++) {
+            Book book = books.get(i);
+            if (book.getId() == updatedBook.getId()) {
+                books.set(i, updatedBook);
+                break;
+            }
+        }
+
+        saveAllBooks(books);
+    }
+
+    public void deleteBook(int id) {
+        List<Book> books = readAllBooks();
+
+        for (int i = 0; i < books.size(); i++) {
+            Book book = books.get(i);
+            if (book.getId() == id) {
+                books.remove(i);
+                break;
+            }
+        }
+
+        saveAllBooks(books);
+    }
+
+    /**
+     * Sobreescribe el contenido del archivo <code>books.psv</code> con la nueve lista de libros
+     * que se pase por parámetro.
+     * <p>
+     * Se suele llamar siempre que se manipulen los datos del archivo mencionado anteriormente para
+     * asegurar la persistencia de los datos.
+     * @param books la nueva lista de libros que será almacenada.
+     */
+    private void saveAllBooks(List<Book> books) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_PATH))) {
+            for (Book book : books) {
+                String bookData = getBookDataString(book);
+                writer.println(bookData);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getBookDataString(Book book) {
+        return String.format("%d|%s|%s|%s|%d|%d|%s|%s|%d|%s",
+                book.getId(), book.getTitle(), book.getAuthor(), book.getGenre(), book.getNumberOfPages(),
+                book.getPublishedDate(), book.getLanguage(), book.getIsbn(), book.getRating(), book.getFragment());
+    }
+
+    private Book parseBookData(String bookData) {
+        String[] parts = bookData.split("\\|");
+        int id = Integer.parseInt(parts[0]);
+        String title = parts[1];
+        String author = parts[2];
+        String genre = parts[3];
+        int numberOfPages = Integer.parseInt(parts[4]);
+        int publishedDate = Integer.parseInt(parts[5]);
+        String language = parts[6];
+        String isbn = parts[7];
+        int rating = Integer.parseInt(parts[8]);
+        String fragment = parts[9];
+
+        return new Book(id, title, author, genre, numberOfPages, publishedDate, language, isbn, rating, fragment);
+    }
+}
