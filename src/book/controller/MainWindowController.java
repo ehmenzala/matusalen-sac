1b07f7c2d7cc2b5095c1b804ee5e6841beecd639
ehmenzala <ehmenzala@gmail.com>
2023-05-30 16:11:45 -0500

    Set up MainWindow controller

--- src/book/controller/MainWindowController.java
+++ src/book/controller/MainWindowController.java
@@ -1,5 +1,44 @@
 package book.controller;
 
-public class BookController {
+import book.model.Book;
+import book.model.BookQuery;
+import book.model.BookShelf;
+import book.view.MainWindow;
+
+public class MainWindowController {
     
+    private MainWindow mainWindow;
+    private BookShelf bookShelf;
+    
+    public MainWindowController(MainWindow mainWindow, BookShelf bookShelf) {
+        this.mainWindow = mainWindow;
+        this.bookShelf = bookShelf;
+        setActionListeners();
+    }
+    
+    public void showWindow() {
+        mainWindow.setVisible(true);
+    }
+    
+    private void setActionListeners() {
+        mainWindow.onSearchClick((e) -> {
+            BookQuery bq = new BookQuery(
+                    mainWindow.getSearchType(),
+                    mainWindow.getSearchTerm());
+            Book[] searchResults = bookShelf.searchBook(bq);
+            mainWindow.createResultItems(searchResults);
+        });
+        
+        mainWindow.onOrderClick((e) -> System.out.println("Click Order"));
+    }
+    
+    // For demo only
+    public static void main(String[] args) {
+        
+        MainWindow mainWindow = new MainWindow();
+        BookShelf bs = new BookShelf();
+        MainWindowController controller = new MainWindowController(mainWindow, bs);
+        
+        controller.showWindow();
+    }
 }
