0e953b929ed82e99ee61637250cb14a4561ce866
ehmenzala <ehmenzala@gmail.com>
2023-05-30 16:08:52 -0500

    Add action listener handlers and add method to remove result entries

--- src/book/view/MainWindow.java
+++ src/book/view/MainWindow.java
@@ -4,8 +4,7 @@ import book.model.Book;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import java.awt.Image;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.ParallelGroup;
@@ -16,7 +15,6 @@ public class MainWindow extends javax.swing.JFrame {
     public MainWindow() {
         initComponents();
         colocarImagen(lblTutorialImage, "./src/images/tutorial-image.png");
-        createResultItems();
     }
 
     @SuppressWarnings("unchecked")
@@ -112,7 +110,7 @@ public class MainWindow extends javax.swing.JFrame {
         pnlNosotros.setLayout(pnlNosotrosLayout);
         pnlNosotrosLayout.setHorizontalGroup(
             pnlNosotrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 680, Short.MAX_VALUE)
+            .addGap(0, 685, Short.MAX_VALUE)
         );
         pnlNosotrosLayout.setVerticalGroup(
             pnlNosotrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -174,75 +172,44 @@ public class MainWindow extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainWindow().setVisible(true);
-            }
-        });
+    
+    public void onSearchClick(ActionListener listener) {
+        btnSearch.addActionListener(listener);
+    }
+    
+    public void onOrderClick(ActionListener listener) {
+        btnOrder.addActionListener(listener);
     }
 
-    private void colocarImagen(JLabel lbl, String ruta) {
-        ImageIcon image = new ImageIcon(ruta);
-        Icon icono = new ImageIcon(image.getImage().getScaledInstance(lbl.getWidth(), lbl.getHeight(), Image.SCALE_DEFAULT));
-        lbl.setIcon(icono);
-        this.repaint();
+    public String getSearchTerm() {
+        return txtSearchBox.getText().toLowerCase();
+    }
+    
+    public String getSearchType() {
+        return ((String) cbxCriteria.getSelectedItem()).toLowerCase();
     }
 
-    public void createResultItems() {
+    public void createResultItems(Book[] books) {
+        cleanResultEntries();
+        
         GroupLayout pnlResultsLayout = new GroupLayout(pnlResults);
         ParallelGroup hResultados = pnlResultsLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false);
         SequentialGroup vResultados = pnlResultsLayout.createSequentialGroup();
         pnlResults.setLayout(pnlResultsLayout);
         
-        ArrayList<Book> books = new ArrayList<>(Arrays.asList(
-                new Book(1, "The One Thing", "Gary Keller", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento"),
-                new Book(2, "Cien años de soledad", "No sé quien", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento"),
-                new Book(3, "Poder sin Límites", "Anthony Robbins", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento"),
-                new Book(4, "Amor en tiempos de cólera", "Gabrial García Márquez", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento"),
-                new Book(5, "Sálvense quien pueda", "Andres Oppenheimer", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento"),
-                new Book(6, "Originales", "Adam Grant", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento"),
-                new Book(7, "El Espía del Inca", "Dummet", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento"),
-                new Book(8, "Deep Work", "Cal Newport", "Self-development", 271, 2011, "English", "1-8888-8888-8", 5, "Este es un fragmento")
-        ));
-        
-        int numItems = books.size();
+        int noOfItems = books.length;
 
-        for (int i = 0; i < numItems; i++) {
-            Book book = books.get(i);
+        for (int i = 0; i < noOfItems; i++) {
+            Book book = books[i];
             BookResultPanel resultItem;
-            
+
             if (i == 0) {
                 resultItem = new BookResultPanel(book, BookResultPanel.PanelType.LONG);
             } else {
                 resultItem = new BookResultPanel(book, BookResultPanel.PanelType.DEFAULT);
             }
 
-            if (i < numItems) {
+            if (i < noOfItems) {
                 if ( i == 0) {
                     vResultados.addGap(16, 16, 16)
                         .addComponent(resultItem, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
@@ -267,14 +234,29 @@ public class MainWindow extends javax.swing.JFrame {
                 pnlResultsLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(pnlResultsLayout.createSequentialGroup()
                                 .addGap(16, 16, 16)
-                                .addGroup(hResultados)
+                                .addGroup(hResultados) // HORIZONTAL GROUP
                                 .addContainerGap(109, Short.MAX_VALUE))
         );
 
         pnlResultsLayout.setVerticalGroup(
                 pnlResultsLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(vResultados)
+                        .addGroup(vResultados) // VERTICAL GROUP
         );
+    }
+    
+    private void cleanResultEntries() {
+        pnlResults.removeAll();
+        pnlResults.revalidate();
+        pnlResults.repaint();
+        
+        resultsScrollPane.setViewportView(pnlResults);
+    }
+    
+    private void colocarImagen(JLabel lbl, String ruta) {
+        ImageIcon image = new ImageIcon(ruta);
+        Icon icono = new ImageIcon(image.getImage().getScaledInstance(lbl.getWidth(), lbl.getHeight(), Image.SCALE_DEFAULT));
+        lbl.setIcon(icono);
+        this.repaint();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
