8e11a720a7c59e26a489e840935c07a977fa7e6f
ehmenzala <ehmenzala@gmail.com>
2023-06-01 13:29:09 -0500

    Add behavior to Ordenar button

--- src/book/controller/MainWindowController.java
+++ src/book/controller/MainWindowController.java
@@ -4,41 +4,66 @@ import book.model.Book;
 import book.model.BookQuery;
 import book.model.BookShelf;
 import book.view.MainWindow;
+import javax.swing.JOptionPane;
 
 public class MainWindowController {
-    
+
     private MainWindow mainWindow;
     private BookShelf bookShelf;
-    
+    private Book[] searchResults = {};
+
     public MainWindowController(MainWindow mainWindow, BookShelf bookShelf) {
         this.mainWindow = mainWindow;
         this.bookShelf = bookShelf;
         setActionListeners();
     }
-    
+
     public void showWindow() {
         mainWindow.setVisible(true);
     }
-    
+
     private void setActionListeners() {
+
         mainWindow.onSearchClick((e) -> {
+            System.out.println("Clcik en Search");
             BookQuery bq = new BookQuery(
                     mainWindow.getSearchType(),
                     mainWindow.getSearchTerm());
-            Book[] searchResults = bookShelf.searchBook(bq);
+            searchResults = bookShelf.searchBook(bq);
+            System.out.println("# SEARCH RESULTS");
+            System.out.println("# SEARCH LENGHT: " + searchResults.length);
+            for (Book book : searchResults) {
+                System.out.println("> Title: " + book.getTitle());
+            }
             mainWindow.createResultItems(searchResults);
         });
+
+        mainWindow.onOrderClick((e) -> {
+            String sortCriteria = mainWindow.getSearchType();
+
+            if (searchResults.length != 0) {
+                Book[] sortedResults = bookShelf.sortBooksByCriteria(searchResults, sortCriteria);
+                mainWindow.createResultItems(sortedResults);
+            } else {
+                showErrorMessage(
+                        "No se encontraron resultados de búsqueda",
+                        "Error de ordenamiento");
+            }
+        });
         
-        mainWindow.onOrderClick((e) -> System.out.println("Click Order"));
     }
-    
+
+    private void showErrorMessage(String message, String title) {
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
+    }
+
     // For demo only
     public static void main(String[] args) {
-        
+
         MainWindow mainWindow = new MainWindow();
         BookShelf bs = new BookShelf();
         MainWindowController controller = new MainWindowController(mainWindow, bs);
-        
+
         controller.showWindow();
     }
 }
