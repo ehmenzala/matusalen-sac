6a5cb0559586cfee752274384a0ee11b9fb0d711
ehmenzala <ehmenzala@gmail.com>
2023-06-01 18:17:10 -0500

    Change Main.java behavior

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,34 @@
 
 import admin.controller.AdminController;
 import admin.view.AdminLogin;
+import admin.view.AdminManagement;
 import book.controller.MainWindowController;
 import book.model.BookShelf;
 import book.view.MainWindow;
+import presentation.Welcome;
 
 public class Main {
 
     public static void main(String[] args) {
         
+        // Initial window
+//        new Welcome().setVisible(true);
+        
         // MainWindow
-        MainWindow mainWindow = new MainWindow();
-        BookShelf bs = new BookShelf();
-        MainWindowController controller = new MainWindowController(mainWindow, bs);
-
-        controller.showWindow();
+//        MainWindow mainWindow = new MainWindow();
+//        BookShelf bs = new BookShelf();
+//        MainWindowController controller = new MainWindowController(mainWindow, bs);
+//
+//        controller.showWindow();
 
         // AdminLogin
         AdminLogin al = new AdminLogin();
-        AdminController ctrl = new AdminController(al);
+        AdminManagement am = new AdminManagement();
         
-        ctrl.iniciar();
-        al.setVisible(true);
+        AdminController ctrl = new AdminController(al, am);
+        
+        //ctrl.iniciarLogin();
+        ctrl.iniciarAdminManagement();
 
         
     }
