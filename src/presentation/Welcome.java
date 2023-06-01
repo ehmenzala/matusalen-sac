44150d6263320e725e3e48180b8371f7c6654bc6
ehmenzala <ehmenzala@gmail.com>
2023-06-01 15:55:45 -0500

    Add random phrase generation funcionality

--- src/presentation/Welcome.java
+++ src/presentation/Welcome.java
@@ -3,10 +3,19 @@ package presentation;
 import utilities.MetodoGeneral;
 
 public class Welcome extends javax.swing.JFrame {
+    
+    private String[] phrases = {
+        "Causa es de perder lo seguro ir en busca de lo incierto. — Yoda.",
+        "La felicidad de tu vida depende de la calidad de tus pensamientos. — Marco Aurelio.",
+        "Pensar es fácil, actuar es difícil, y poner los pensamientos de uno mismo en acción es lo más difícil del mundo. — Goethe.",
+        "La verdadera libertad consiste en el dominio absoluto de sí mismo. — Galileo Galilei."
+    };
 
     public Welcome() {
         initComponents();
         MetodoGeneral.colocarImagen(lblfondo, "./src/images/bibliofondo.jpg");
+        int randomIndex = (int) (Math.random() * phrases.length);
+        lblFrase.setText(String.format("<html>«%s»</html>", phrases[randomIndex]));
     }
 
     @SuppressWarnings("unchecked")
@@ -33,7 +42,7 @@ public class Welcome extends javax.swing.JFrame {
         lblMatusalen.setText("Matusalén S.A.C");
 
         btnAdmin.setBackground(new java.awt.Color(0, 0, 204));
-        btnAdmin.setFont(new java.awt.Font("Clarendon BT", 1, 12)); // NOI18N
+        btnAdmin.setFont(new java.awt.Font("Clarendon BT", 1, 14)); // NOI18N
         btnAdmin.setForeground(new java.awt.Color(255, 255, 255));
         btnAdmin.setText("Admnistrador");
         btnAdmin.setToolTipText("");
@@ -43,7 +52,12 @@ public class Welcome extends javax.swing.JFrame {
         lblEslogan.setForeground(new java.awt.Color(255, 255, 255));
         lblEslogan.setText("\"Ayudándote a fortalecer tu conocimiento\"");
 
+        lblFrase.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         lblFrase.setForeground(new java.awt.Color(255, 255, 51));
+        lblFrase.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblFrase.setText("TEXTO DE EJEMPLO");
+        lblFrase.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        lblFrase.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         btnEntrar.setBackground(new java.awt.Color(0, 0, 204));
         btnEntrar.setFont(new java.awt.Font("Clarendon BT", 1, 14)); // NOI18N
@@ -61,18 +75,18 @@ public class Welcome extends javax.swing.JFrame {
                         .addGap(71, 71, 71)
                         .addComponent(lblMatusalen, javax.swing.GroupLayout.PREFERRED_SIZE, 291, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(29, 29, 29)
-                        .addComponent(lblEslogan, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(50, 50, 50)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblFrase, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(btnEntrar, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(btnAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(10, 10, 10)))))
+                        .addComponent(btnEntrar, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(10, 10, 10))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(lblEslogan, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(39, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(lblFrase, javax.swing.GroupLayout.PREFERRED_SIZE, 417, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -81,9 +95,9 @@ public class Welcome extends javax.swing.JFrame {
                 .addComponent(lblMatusalen, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblEslogan, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
-                .addComponent(lblFrase, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblFrase, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 93, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnEntrar, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -97,9 +111,7 @@ public class Welcome extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
