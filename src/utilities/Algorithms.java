c5862943ed7b533725c3bdbf7fce5928e2e99ec3
ehmenzala <ehmenzala@gmail.com>
2023-05-31 19:24:57 -0500

    insertionSort, quicksort & shellSort

--- src/utilities/Algorithms.java
+++ src/utilities/Algorithms.java
@@ -1,91 +1,67 @@
 package utilities;
 
-public class Algorithms {
-    public static int[] OrdenamientoBurbuja(int[] arreglo) {
-        int auxiliar;
-        for (int i = 1; i < arreglo.length; i++) { //i=1 1=2 1=3
-            for (int j = 0; j < arreglo.length - i; j++) { //j=0 j=1 j=2     j=0 j=1    j=0
-                if (arreglo[j] > arreglo[j + 1]) {
-                    auxiliar = arreglo[j];
-                    arreglo[j] = arreglo[j + 1];
-                    arreglo[j + 1] = auxiliar;
-                }
-            }
-        }
-        return arreglo;
-    }
-    
-    
-    public static int[] seleccion(int[] array) {
-        for (int i = 0; i < array.length; i++) { //i=0 i=1 i=2 i=3 
-            int posimenor = i;
-            for (int j = i + 1; j < array.length; j++) { // j=1 j=2 j=3
-                if (array[j] < array[posimenor]) {
-                    posimenor = j;
-                }
-            }
+import java.util.Arrays;
+import java.util.stream.Stream;
 
-            int aux = array[i];
-            array[i] = array[posimenor];
-            array[posimenor] = aux;
-        }
-        return array;
-    }
-    
-    
-    public static void quicksort(int A[], int izq, int der) {
-        int pivote = A[izq], i = izq, j = der;
-        int aux;
-        while (i < j) {
-            while (A[i] >= pivote && i < j) {
-                i++;
-            }
-            while (A[j] < pivote) {
-                j--;
-            }
-            if (i < j) {
-                aux = A[i];
-                A[i] = A[j];
-                A[j] = aux;
-            }
-        }
-        A[izq] = A[j];
-        A[j] = pivote;
-        if (izq < j - 1) {
-            quicksort(A, izq, j - 1);
-        }
-        if (j + 1 < der) {
-            quicksort(A, j + 1, der);
-        }
-    }
-    
-    
-    public static int busquedaSecuencial(String vector[], String elemento){
-        int n=vector.length;
-        int i=0;
-        
-        while (i <=n-1) {
-            if (elemento.equals(vector[i])) {
-                return i;
-            }
-            i=i+1;
-        }
-        return -1;
-    }
-    
-    
-    public static String [] insercion(String [] arre ){
-        for (int i = 1; i < arre.length; i++) {
-            String value=arre[i];
+public class Algorithms {
+
+    public static String[] insertionSortStrings(String[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            String currentVal = arr[i];
             int j = i;
-            
-            while (j>0 && arre[j-1].compareTo(value)>0) {
-                arre[j] = arre[j-1];
+
+            while (j > 0 && (arr[j - 1].compareTo(currentVal)) > 0) {
+                arr[j] = arr[j - 1];
                 j--;
             }
-            arre[j]=value;
+            arr[j] = currentVal;
         }
-        
-        return arre;
+        return arr;
+    }
+
+    public static int[] quicksort(int[] arr) {
+        if (arr.length < 2) {
+            return arr;
+        } else {
+            int pivotIndex = arr.length / 2;
+            int pivot = arr[pivotIndex];
+
+            int[] less = Arrays.stream(arr)
+                    .filter(element -> element <= pivot && element != pivot)
+                    .toArray();
+            int[] greater = Arrays.stream(arr)
+                    .filter(element -> element > pivot)
+                    .toArray();
+
+            return Stream.of(
+                    quicksort(less),
+                    new int[]{pivot},
+                    quicksort(greater))
+                    .flatMapToInt(Arrays::stream)
+                    .toArray();
+        }
+    }
+    
+    public static String[] shellSortStrings(String[] arr) {
+        int salto, i;
+        String aux;
+        boolean cambios;
+
+        for (salto = arr.length / 2; salto != 0; salto /= 2) {
+            cambios = true;
+            while(cambios) {
+                cambios = false;
+                for (i = salto; i < arr.length; i++) {
+                    if ((arr[i - salto].compareTo(arr[i])) > 0) {
+                        aux = arr[i];
+                        arr[i] = arr[i - salto];
+                        arr[i - salto] = aux;
+                        cambios = true;
+                    }
+
+                }
+            }
+        }
+        return arr;
     }
 }
