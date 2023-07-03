96582c5b616c77ea49aa22352b9a8b85c26b69f7
ehmenzala <ehmenzala@gmail.com>
2023-07-03 18:13:28 -0500

    Add authors graph generator

---
+++ src/author/model/AuthorGraphGenerator.java
@@ -0,0 +1,55 @@
+package author.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthorGraphGenerator {
+
+    private final Map<String, ArrayList<String>> authors;
+    private final String FILE_PATH = "./src/db/autores.psv";
+
+    public AuthorGraphGenerator() {
+        this.authors = new HashMap<>();
+        fillGraphData();
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public void readGraphData() {
+        for (String author : authors.keySet()) {
+            System.out.println("# " + author);
+            System.out.println(authors.get(author));
+        }
+    }
+
+    private void fillGraphData() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            boolean isFirstAuthor = true;
+            String authorKey = "";
+            ArrayList<String> collaborators = new ArrayList<>();
+
+            while ((line = reader.readLine()) != null) {
+
+                if (line.equals("")) {
+                    isFirstAuthor = true; // Reset isFirstAuthor
+                    authors.put(authorKey, collaborators); // Add new element to authors map
+                    collaborators = new ArrayList<>(); // Create a new instance for collaborators
+                    continue;
+                }
+                
+                if (isFirstAuthor) {
+                    authorKey = line;
+                    isFirstAuthor = false;
+                } else {
+                    collaborators.add(line);
+                }
+            }
+        } catch (Exception e) {}
+    }
+}
